ed5c0fdad72be124874e5bba090f2476ce73cfd6
SaurabhPerane1 <119008712+SaurabhPerane1@users.noreply.github.com>
2024-10-21 02:31:18 +0530

    Update python_flask.py

--- python_flask.py
+++ python_flask.py
@@ -1,1 +1,44 @@
+import mediapipe as mp
+import cv2
 
+#mp methods
+mp_drawing = mp.solutions.drawing_utils
+mp_holistic = mp.solutions.holistic
+
+#cap var to store VideoCapture(0)
+cap = cv2.VideoCapture(0)
+with mp_holistic.Holistic(min_detection_confidence=0.5, min_tracking_confidence=0.5) as holistic:
+    while cap.isOpened():
+        ret, frame = cap.read()
+
+        # Recolor Feed
+        image = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+
+        # Make Detections
+        results = holistic.process(image)
+
+        # Recolor image back to BGR for rendering
+        image = cv2.cvtColor(image, cv2.COLOR_RGB2BGR)
+
+        # Draw face landmarks
+        # mp_drawing.draw_landmarks(image, results.face_landmarks, mp_holistic.FACE_CONNECTIONS)
+
+        # Right hand
+        mp_drawing.draw_landmarks(image, results.right_hand_landmarks, mp_holistic.HAND_CONNECTIONS)
+
+        # Left Hand
+        mp_drawing.draw_landmarks(image, results.left_hand_landmarks, mp_holistic.HAND_CONNECTIONS)
+
+        # Pose Detections
+        mp_drawing.draw_landmarks(image, results.pose_landmarks, mp_holistic.POSE_CONNECTIONS)
+
+        # Window title
+        cv2.imshow('Raw Webcam Feed', image)
+
+        if cv2.waitKey(10) & 0xFF == ord('q'):
+            break
+
+# Release resources when finished
+cap.release()
+#close window
+cv2.destroyAllWindows()
